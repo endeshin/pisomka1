8c660df822bacd0150754535074bfee1229202fe
endeshin <alex.viktorini@outlook.sk>
2024-11-06 10:30:43 +0100

    commit 2

--- src/sections/AuthHomeView.tsx
+++ src/sections/AuthHomeView.tsx
@@ -11,7 +11,7 @@ export default function AuthHomeView({ session }: { session: Session }) {
   return (
     <Container>
       <Typography variant="h4" sx={{ mb: 3 }}>
-        Vitajte, {session?.user?.name || "užívateľ"}! Dáte si poker?
+        Vitajte, {session?.user?.name || "užívateľ"}!
       </Typography>
 
 
--- src/sections/NonAuthHomeView.tsx
+++ src/sections/NonAuthHomeView.tsx
@@ -5,11 +5,8 @@ import { Container, Typography } from "@mui/material";
 export default function NonAuthHomeView() {
   return (
     <Container>
-      <Typography variant="body1" sx={{ mt: 2 }}>
-        Takto si poker nezahráš !
-      </Typography>
       <Typography>
-        Registrujte sa, aby ste mohli zahrať.
+        Registrujte sa.
       </Typography>
     </Container>
   );
